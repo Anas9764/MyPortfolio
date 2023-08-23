59d6bf2866b0d67dc55d6bc3ccd166068de27576
Anas9764 <s19_qureshi_anas@mgmcen.ac.in>
2023-08-23 23:55:20 +0530

    Forntend and backend skills created in skill section

--- src/components/skills/backend.jsx
+++ src/components/skills/backend.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 function backend() {
   return (
-    <div>backend</div>
+    <div className="skills__content">
+    <h3 className="skills__title"></h3>
+
+    <div className="skills__box">
+      <div className="skills__group">
+        <div className="skills__data">
+          <i className="bx bx-bage-check"></i>
+          
+          <div>
+            <h3 className="skills__name"></h3>
+            <span className="skills__level"></span>
+          </div>
+        </div>
+
+        <div className="skills__data">
+          <i className="bx bx-bage-check"></i>
+          
+          <div>
+            <h3 className="skills__name"></h3>
+            <span className="skills__level"></span>
+          </div>
+        </div>
+
+        <div className="skills__data">
+          <i className="bx bx-bage-check"></i>
+          
+          <div>
+            <h3 className="skills__name"></h3>
+            <span className="skills__level"></span>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+   </div>
   )
 }
 
