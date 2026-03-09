9157d836960e96ce9facdfa29fa8a6c71048b5e2
Anas9764 <anasqureshi9764@gmail.com>
2026-03-10 00:06:51 +0530

    updated the skill years

--- website/src/api/index.js
+++ website/src/api/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API = axios.create({ 
+const API = axios.create({
   baseURL: (import.meta.env.VITE_API_URL || 'https://myportfolio-tn8r.onrender.com/api').replace(/\/$/, '') + '/'
 });
 
 export const getPortfolioData = () => API.get('portfolio/data');
+
 export const trackVisit = () => API.post('portfolio/analytics/track');
 export const sendMessage = (data) => API.post('portfolio/messages', data);
 
